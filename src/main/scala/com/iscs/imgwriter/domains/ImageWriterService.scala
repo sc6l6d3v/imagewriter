9cc26b0eec0b3d6026a3ccf59c0db26bf743267e
sc6l6d3v <hk-vndr@iscs-i.com>
2020-11-19 17:24:12 -0500

    Limit content by width/height of bounding image.

--- src/main/scala/com/iscs/imgwriter/domains/ImageWriterService.scala
+++ src/main/scala/com/iscs/imgwriter/domains/ImageWriterService.scala
@@ -1,11 +1,11 @@
 package com.iscs.imgwriter.domains
 
+import java.awt.Graphics
 import java.awt.image.BufferedImage
 import java.io.{ByteArrayOutputStream, IOException}
 import java.net.URL
 
 import cats.effect.{Concurrent, ConcurrentEffect}
-import cats.effect.implicits._
 import cats.implicits._
 import fs2.Stream
 import org.log4s.getLogger
@@ -17,6 +17,7 @@ class ImageWriterService[F[_]: Concurrent](implicit F: ConcurrentEffect[F]) {
   private val lenaImage = "http://kevinrye.net/index_files/lenna-so0308derberg-1972_620.jpg"
   private val dummyImage = new BufferedImage(256, 256, BufferedImage.TYPE_INT_RGB)
   private val yOffset = 30
+  private val fontSize = 20f
 
   def updateImage(text: String, x: Int, y: Int): Stream[F, Byte] = for {
     maybeImage <- Stream.eval(getImage(lenaImage))
@@ -56,10 +57,23 @@ class ImageWriterService[F[_]: Concurrent](implicit F: ConcurrentEffect[F]) {
   } yield s
 
   private def withText(img: BufferedImage, text: String, x: Int, y: Int): BufferedImage = {
+    def embedText(text: String, g: Graphics, x: Int, y: Int): Unit =  {
+      g.setFont(g.getFont.deriveFont(fontSize))
+      g.drawString(text, x, y)
+      g.dispose()
+    }
+
+    val imgWidth = img.getWidth
+    val imgHeight = img.getHeight
     val g = img.getGraphics
-    g.setFont(g.getFont.deriveFont(30f))
-    g.drawString(text, x, y)
-    g.dispose()
+    val fontMetrics = g.getFontMetrics(g.getFont.deriveFont(fontSize))
+    val stringHeight = fontMetrics.stringWidth(text)
+    val stringWidth = fontMetrics.getStringBounds(text, g)
+    if (y + stringHeight < imgHeight && x + stringWidth.getWidth.toInt < imgWidth) {
+      embedText(text, g, x, y)
+    } else {
+      embedText("Too long", g, x, y)
+    }
     img
   }
 }

882347e764e53c4ee6098299cf062d4d40c6228a
sc6l6d3v <hk-vndr@iscs-i.com>
2020-12-15 11:56:10 -0500

    Add CORS for react front end.

--- src/main/scala/com/iscs/imgwriter/routes/Routes.scala
+++ src/main/scala/com/iscs/imgwriter/routes/Routes.scala
@@ -6,6 +6,8 @@ import com.typesafe.scalalogging.Logger
 import fs2.Stream
 import org.http4s._
 import org.http4s.dsl.Http4sDsl
+import org.http4s.server.middleware.{CORS, CORSConfig}
+import scala.concurrent.duration._
 
 import scala.util.Try
 
@@ -15,10 +17,20 @@ object Routes {
   private val topLeftX = 100
   private val topLeftY = 180
 
+  private val reactOrigin = "http://localhost:3000"
+  private val methods = Set("GET")
+  private val methodConfig = CORSConfig(
+    anyOrigin = false,
+    allowCredentials = true,
+    maxAge = 1.day.toSeconds,
+    allowedMethods = Some(methods),
+    allowedOrigins = Set(reactOrigin)
+  )
+
   def imageRoutes[F[_]: Sync: Concurrent](I: ImageWriterService[F]): HttpRoutes[F] = {
     val dsl = new Http4sDsl[F]{}
     import dsl._
-    HttpRoutes.of[F] {
+    val service = HttpRoutes.of[F] {
       case req @ GET -> Root / "static" / index / txt =>
         if (txt.size > inputLimit)
           Ok("Text overflow")
@@ -29,5 +41,6 @@ object Routes {
             img <- I.updateImage(txt, topLeftX, topLeftY, imageNdx)
           } yield img)
     }
+    CORS(service, methodConfig)
   }
 }
